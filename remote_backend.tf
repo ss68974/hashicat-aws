eb87c139d4729eda0a5c412f3a0d90d216d65371
Sushant Samantaray <sushanta.samantaray@gmail.com>
2023-08-18 23:08:58 +0530

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-<<<<<<< HEAD
     organization = "sushant-aws-org"
-=======
-    organization = "sushanta-training"
->>>>>>> 3062964f3e8b48ad533b3ae91fc3c5f85f930072
     workspaces {
       name = "hashicat-aws"
     }
