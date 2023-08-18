51afddc09c263a052abd33a60f6d231fddad8a11
ss68974 <sushanta.samantaray@gmail.com>
2023-08-18 18:26:17 +0000

    s3 task

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "isv-lab-my-s3-bucket"
   acl    = "private"
   bucket_prefix = "sushant"
 
